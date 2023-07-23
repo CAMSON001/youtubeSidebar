755017531accec559e4a6df827844146139d4776
Camara Vamoro <cvamorocamson@gmail.com>
2023-07-23 18:03:56 +0100

    for committment

---
+++ index.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector(".menu")
+let sideBar = document.querySelector(".sideBar")
+let nam = document.querySelector(".name")
+let nam2 = document.querySelector(".name")
+
+
+
+
+  function toggle(){
+    sideBar.classList.toggle("active")
+    nam.classList.toggle("active")
+    nam2.classList.toggle("active")
+}
