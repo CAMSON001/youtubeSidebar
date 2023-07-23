789b3419a34e40f6df433ba1c7f310bfd8985097
Camara Vamoro <cvamorocamson@gmail.com>
2023-07-23 22:17:14 +0100

    endOfClone

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 let menu = document.querySelector(".menu")
 let sideBar = document.querySelector(".sideBar")
 let nam = document.querySelector(".name")
-let nam2 = document.querySelector(".name")
+let name1 = document.querySelector(".name1")
+let name2 = document.querySelector(".name2")
+let name3 = document.querySelector(".name3")
+let name4 = document.querySelector(".name4")
+let name5= document.querySelector(".name5")
+let name6 = document.querySelector(".name6")
+let name7= document.querySelector(".name7")
+let name8 = document.querySelector(".name8")
 
-
-
+let info = document.querySelector(".coordonate")
+let arrow = document.querySelector(".arrow")
 
   function toggle(){
     sideBar.classList.toggle("active")
     nam.classList.toggle("active")
-    nam2.classList.toggle("active")
+    name1.classList.toggle("active")
+    name2.classList.toggle("active")
+    name3.classList.toggle("active")
+    name4.classList.toggle("active")
+    name5.classList.toggle("active")
+    name6.classList.toggle("active")
+    name7.classList.toggle("active")
+    name8.classList.toggle("active")
+    info.classList.toggle("active")
+    arrow.classList.toggle("active")
+    
 }
+
+
